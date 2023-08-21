7fa163f4c7c25f2a9da968e4859b7f94e186b8a4
Benjameslane <ben.james.lane9>
2023-08-21 14:46:52 -0600

    added NavBar

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar(){
+    return <nav className="nav">
+        <a href="/" className="site-title">SocialFeed</a>
+        <ul>
+            <li>
+                <a href="about">About</a>
+            </li>
+        </ul>
+    </nav>
+}
