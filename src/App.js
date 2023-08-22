1b8adbe37248bbb0be044154921a9fb6aaad6e1a
Benjameslane <ben.james.lane9>
2023-08-22 12:56:11 -0600

    added post options

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
-import NavBar from './NavBar/NavBar';
+import NavBar from './Components/NavBar/NavBar'
+import PostList from './Components/PostList/PostList'
+import Post from './Components/Post'
 
 
 
 function App() {
 
+const [entries, setEntries] = useState([{Name: 'Benjamin Lane', Post: 'My first post!'}, {Name: 'Kyra Mourey', Post: 'I love SocialFeed!'}])
 
-  return <NavBar /> 
+function addNewPost(entry){
+  let tempEntries = [...entries, entry];
+  setEntries(tempEntries);
+}
 
+
+  return (
+    <div>
+  <NavBar />
+  <PostList parentEntries={entries} />
+  <Post addNewPostProperty={addNewPost}/>
+  
+  </div>
+
+  );
   
 }
 
---
+++ src/Components/Post.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+
+const Post = (props) => {
+
+    const [Name, setName] = useState('');
+    const [Post, setPost] = useState('');
+
+    function handleSubmit(event){
+        event.preventDefault();
+        let newEntry = {
+            Name: Name,
+            Post: Post
+        }
+        console.log(newEntry)
+        props.addNewPostProperty(newEntry)
+    }
+
+    return (  
+        <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input type='text'value={Name} onChange={(event) => setName(event.target.value)}/>
+            <label>Post</label>
+            <input type='text' value={Post} onChange={(event) => setPost(event.target.value)}/>
+            <button type='submit'>Add</button>
+        </form>
+    );
+}
+ 
+export default Post;
+
+
+
+
+
+
+
+
+
+//commented out is from a lexture, May be viable later on 
+
+//<form action="" method="post">
+    //<label for="first_name">First Name:</label>
+    //<input type="text" name="first_name">
+    //<label for="last_name">Last Name:</label>
+   // <input type="text" name="last_name">
+//</form>
